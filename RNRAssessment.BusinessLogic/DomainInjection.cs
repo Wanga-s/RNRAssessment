2f2b7db58218591103c3980490ac830385d44957
wanga <wtsewana@gmail.com>
2023-12-24 13:12:49 +0200

    Added stubs for BusinessLogic

---
+++ RNRAssessment.BusinessLogic/DomainInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace RNRAssessment.BusinessLogic
+{
+    public static class DomainInjection
+    {
+        public static void InjectBusinessLogic(this IServiceCollection Services)
+        {
+
+        }
+    }
+}
