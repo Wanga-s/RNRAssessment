4a3210288886d9d2051f9e15a7013ad6f15aef8b
wanga <wtsewana@gmail.com>
2023-12-24 17:22:48 +0200

    Updated front-end

--- RNRAssessment.UI/Controllers/HomeController.cs
+++ RNRAssessment.UI/Controllers/HomeController.cs
@@ -1,32 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RNRAssessment.UI.Models;
+using RNRAssessment.UI.Services;
 using System.Diagnostics;
 
 namespace RNRAssessment.UI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return RedirectToAction("Index","Breakdown");
         }
     }
 }
